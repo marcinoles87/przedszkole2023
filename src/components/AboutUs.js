8d5a9c41aefbf3b70b2d35544858584afd9c3645
marcinoles87 <marcinoles87@gmail.com>
2023-10-29 09:50:36 +0100

    work with contanct

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import Container from 'react-bootstrap/esm/Container'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 function AboutUs() {
   return (
-    <div id='about'>AboutUs</div>
+    <Container id='about'>
+       <Row>
+        <Col>
+          <h1>O naszym przedszkolu ...</h1>
+          <p>Przedszkole usytuowane jest w budynku piętrowym, wolnostojącym. Dysponuje kilkoma jasnymi obszernymi salami zajęć, łazienkami i dużą szatnią. Na naszym terenie znajduje się piękny, zmodernizowany ogród bogaty w zieleń. Dodatkowo posiadamy bogatą bazę dydaktyczną oraz sprzęt sportowy.Naszym celem jest wspomaganie i ukierunkowanie rozwoju dziecka zgodnie z jego wrodzonym potencjałem w relacjach ze środowiskiem społeczno-kulturowym i przyrodniczym.</p>
+        </Col>
+       </Row>
+      </Container>
   )
 }
 
