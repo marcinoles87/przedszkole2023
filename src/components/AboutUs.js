0e3ee1c1abd636f2209493d51c1453160da60bf9
marcinoles87 <marcinoles87@gmail.com>
2023-10-30 15:47:08 +0100

    work with contanct

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -9,9 +9,11 @@ function AboutUs() {
     <Container id='about'>
        <Row>
         <Col>
-          <h1>O naszym przedszkolu ...</h1>
-          <p>Przedszkole usytuowane jest w budynku piętrowym, wolnostojącym. Dysponuje kilkoma jasnymi obszernymi salami zajęć, łazienkami i dużą szatnią. Na naszym terenie znajduje się piękny, zmodernizowany ogród bogaty w zieleń. Dodatkowo posiadamy bogatą bazę dydaktyczną oraz sprzęt sportowy.Naszym celem jest wspomaganie i ukierunkowanie rozwoju dziecka zgodnie z jego wrodzonym potencjałem w relacjach ze środowiskiem społeczno-kulturowym i przyrodniczym.</p>
-        </Col>
+          <div className='about-info'>
+            <h1>O naszym przedszkolu ...</h1>
+            <p>Przedszkole usytuowane jest w budynku piętrowym, wolnostojącym. Dysponuje kilkoma jasnymi obszernymi salami zajęć, łazienkami i dużą szatnią. Na naszym terenie znajduje się piękny, zmodernizowany ogród bogaty w zieleń. Dodatkowo posiadamy bogatą bazę dydaktyczną oraz sprzęt sportowy.Naszym celem jest wspomaganie i ukierunkowanie rozwoju dziecka zgodnie z jego wrodzonym potencjałem w relacjach ze środowiskiem społeczno-kulturowym i przyrodniczym.</p>
+        </div> 
+       </Col>
        </Row>
       </Container>
   )
