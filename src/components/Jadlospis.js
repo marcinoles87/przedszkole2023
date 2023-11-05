0e21cd32a3d73775d869cf2d23b62c25b217fcc0
marcinoles87 <marcinoles87@gmail.com>
2023-11-05 11:27:41 +0100

    work with food

--- src/components/Jadlospis.js
+++ src/components/Jadlospis.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Col from 'react-bootstrap/esm/Col'
 import Row from 'react-bootstrap/esm/Row'
+import Table from 'react-bootstrap/esm/Table'
 import Container from 'react-bootstrap/esm/Container'
 
 function Jadlospis() {
   return (
     <Container>
-      <Row>
+      {/* <Row>
         <Col>
         <h1>Jadlospis w tym tygodniu :</h1>
         <ul>Poniedzialek
@@ -15,8 +16,60 @@ function Jadlospis() {
           <li>zupa kalafiorowa , ziemniaki , kotlet , surowka</li>
         </ul>
         </Col>
-      </Row>
+      </Row> */}
+
+
+        <Table striped bordered hover variant="dark" style={{textAlign:'center'}}>
+            <thead> Poniedzialek
+              <tr>
+                <th>Dzień</th>
+                <th>Sniadanie</th>
+                <th>Obiad</th>
+                <th>Kolacja</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th>Poniedzialek</th>
+                <th>jablko , mus , herbatka</th>
+                <th>jalbko , chrupki kukurydziane , napoj</th>
+                <th>zupa kalafiorowa , ziemniaki , kotlet , surowka</th>
+              </tr>
+
+              <tr>
+                <th>Wtorek</th>
+                <th>jablko , mus , herbatka</th>
+                <th>jalbko , chrupki kukurydziane , napoj</th>
+                <th>zupa kalafiorowa , ziemniaki , kotlet , surowka</th>
+              </tr>
+
+              <tr>
+                <th>Sroda</th>
+                <th>jablko , mus , herbatka</th>
+                <th>jalbko , chrupki kukurydziane , napoj</th>
+                <th>zupa kalafiorowa , ziemniaki , kotlet , surowka</th>
+              </tr>
+
+              <tr>
+                <th>Czwartek</th>
+                <th>jablko , mus , herbatka</th>
+                <th>jalbko , chrupki kukurydziane , napoj</th>
+                <th>zupa kalafiorowa , ziemniaki , kotlet , surowka</th>
+              </tr>
+
+              <tr>
+                <th>Piatek</th>
+                <th>jablko , mus , herbatka</th>
+                <th>jalbko , chrupki kukurydziane , napoj</th>
+                <th>zupa kalafiorowa , ziemniaki , kotlet , surowka</th>
+              </tr>
+
+             
+            </tbody>
+        </Table>
     </Container>
+
+    
   )
 }
 
