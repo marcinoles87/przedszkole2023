57f4008cdaf597d7a7f9d4a3bbb1b1ce505f5321
marcinoles87 <marcinoles87@gmail.com>
2023-11-06 15:39:11 +0100

    work with modal

--- src/App.js
+++ src/App.js
@@ -8,12 +8,20 @@ import Galeria from './components/Galeria';
 import Jadlospis from './components/Jadlospis.js';
 import Main from './components/Main';
 import Navbars from './components/Navbars.js'
+import Button from 'react-bootstrap/esm/Button.js';
 
 
 function App() {
+
+  const handleOnClick = () => {
+    window.scrollTo(0,0)
+  }
+
+
   return (
     <div className="App">
       <Navbars></Navbars>
+      <Button onClick={handleOnClick} className='arrow-up' style={{width:'40px' , height:'40px' , backgroundColor:'black' , borderRadius:'30px'}}><i className="fa fa-arrow-up 2xl"></i></Button>
       <Main></Main>
       <Actual></Actual>
       <AboutUs></AboutUs>
