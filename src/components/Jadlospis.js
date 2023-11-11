98f1ea14d305c78806d5f75101394571d65fca37
marcinoles87 <marcinoles87@gmail.com>
2023-11-11 08:52:37 +0100

    work with main

--- src/components/Jadlospis.js
+++ src/components/Jadlospis.js
@@ -3,21 +3,20 @@ import Col from 'react-bootstrap/esm/Col'
 import Row from 'react-bootstrap/esm/Row'
 import Table from 'react-bootstrap/esm/Table'
 import Container from 'react-bootstrap/esm/Container'
-import page2 from './pages/jadlospis.html'
+
 
 function Jadlospis() {
   return (
     <Container className='jadlospis'>
  
         <h1>Jadłospis</h1>
-        <a href={page2}>kliknij aby obejrzec caly tydzien</a>
         <Table striped bordered hover variant="success" style={{textAlign:'center'}}>
             <thead> 
               <tr>
                 <th>Dzień</th>
                 <th>Sniadanie</th>
+                <th>II Sniadanie</th>
                 <th>Obiad</th>
-                <th>Kolacja</th>
               </tr>
             </thead>
             <tbody>
---
+++ src/components/pages/Dania.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Dania() {
+  return (
+    <div>jadlospis</div>
+  )
+}
+
+export default Dania
