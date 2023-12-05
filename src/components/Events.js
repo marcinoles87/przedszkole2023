bd527b755e360ed63cfd65a55ebe7bd10ccc4918
marcinoles87 <marcinoles87@gmail.com>
2023-12-05 16:52:55 +0100

    work with events section

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,9 +1,46 @@
 import React from 'react'
 
+const event = [ 
+    {
+        name : 'wyjscie do kina' ,
+        data : '22-12-2023' ,
+        month : 'Grudzien'
+    } , 
+
+    {
+        name : 'wyjscie do kina' ,
+        data : '22-12-2023' ,
+        month : 'Grudzien'
+    },
+    
+    {
+        name : 'wyjscie do kina' ,
+        data : '22-12-2023' ,
+        month : 'Grudzien'
+    } ,
+
+    {
+        name : 'wyjscie do kina' ,
+        data : '22-12-2023' ,
+        month : 'Grudzien'
+    }
+]
+
 function Events() {
   return (
     <div className='events-container'>
         <h1>Nadchodzące wydarzenia w naszym przedszkolu</h1>
+        <div className='events-event'>
+            <h2>Grudzien : </h2>
+            {event.map( (item) => {
+                return(
+                    <>
+                     <h3>{item.month}</h3>
+                     <p>{item.data} - {item.name}</p>
+                    </>
+                )
+            })}
+        </div>
     </div>
   )
 }
