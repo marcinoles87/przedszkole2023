bfff595ae0f095319eb9ce73da6423daf0556f09
marcinoles87 <marcinoles87@gmail.com>
2023-12-12 20:51:48 +0100

    work with grup

--- src/components/pages/Grupa1.js
+++ src/components/pages/Grupa1.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './styleGrup.css'
+import img1 from '/web frontend/projekty React/przedszkole2023/src/img/kids.png'
 
 
 function Grupa1() {
@@ -10,6 +11,14 @@ function Grupa1() {
       name : 'Wyjscie do kina' ,
       data : '12-12-2023' ,
       description : ' lorem ipsum lorem ipsum lorem ipsum lorem ipsum vlorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum' ,
+      img1 : img1
+    },
+
+    {
+      name : 'Wyjscie do kina' ,
+      data : '12-12-2023' ,
+      description : ' lorem ipsum lorem ipsum lorem ipsum lorem ipsum vlorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum' ,
+      img1 : img1
 
     },
 
@@ -17,6 +26,7 @@ function Grupa1() {
       name : 'Wyjscie do kina' ,
       data : '12-12-2023' ,
       description : ' lorem ipsum lorem ipsum lorem ipsum lorem ipsum vlorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum' ,
+      img1 : img1
 
     },
 
@@ -24,21 +34,16 @@ function Grupa1() {
       name : 'Wyjscie do kina' ,
       data : '12-12-2023' ,
       description : ' lorem ipsum lorem ipsum lorem ipsum lorem ipsum vlorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum' ,
-
-    },
-
-    {
-      name : 'Wyjscie do kina' ,
-      data : '12-12-2023' ,
-      description : ' lorem ipsum lorem ipsum lorem ipsum lorem ipsum vlorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum' ,
+      img1 : img1
 
     }
   ]
   return (
     <>
-    <div>Grupa1</div>
+    
     <div className='group-header'>
       <h2>Co nowego w grupie 1</h2>
+      <img></img>
 
     </div>
     <div className='group-container'>
@@ -48,6 +53,7 @@ function Grupa1() {
            <h1>{item.name}</h1>
            <h3>{item.data}</h3>
            <p>{item.description}</p>
+          <img src={item.img1}></img>
           </>
         )
       })}
