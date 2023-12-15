0d97cf36246f80a05726267f07e84b487812f7d8
marcinoles87 <marcinoles87@gmail.com>
2023-12-15 05:43:39 +0100

    add group

--- src/components/pages/Grupa2.js
+++ src/components/pages/Grupa2.js
@@ -1,8 +1,65 @@
-import React from 'react'
+import React from 'react';
+import './styleGrup.css';
+import img1 from '/web frontend/projekty React/przedszkole2023/src/img/kids.png';
+import { Link } from 'react-router-dom';
 
 function Grupa2() {
+  
+  const wydarzenia = [ 
+    {
+      name : 'Wyjscie do kina' ,
+      data : '12-12-2023' ,
+      description : ' lorem ipsum lorem ipsum lorem ipsum lorem ipsum vlorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum' ,
+      img1 : img1
+    },
+
+    {
+      name : 'Wyjscie do kina' ,
+      data : '12-12-2023' ,
+      description : ' lorem ipsum lorem ipsum lorem ipsum lorem ipsum vlorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum' ,
+      img1 : img1
+
+    },
+
+    {
+      name : 'Wyjscie do kina' ,
+      data : '12-12-2023' ,
+      description : ' lorem ipsum lorem ipsum lorem ipsum lorem ipsum vlorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum' ,
+      img1 : img1
+
+    },
+
+    {
+      name : 'Wyjscie do kina' ,
+      data : '12-12-2023' ,
+      description : ' lorem ipsum lorem ipsum lorem ipsum lorem ipsum vlorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum' ,
+      img1 : img1
+
+    }
+  ]
   return (
-    <div>Grupa2</div>
+    <>
+    
+    <div className='group-header'>
+      <h2>Co nowego w grupie 2</h2>
+     
+
+    </div>
+    <div className='group-container'>
+      {wydarzenia.map( (item, index) => {
+        return(
+          <div className='group-element' key={index}>
+           <h1>{item.name}</h1>
+           <h3>{item.data}</h3>
+           <p>{item.description}</p>
+          <img src={item.img1}></img>
+          </div>
+        )
+      })}
+    </div>
+   <button><Link to={'/'}>powrot do strony glownej</Link></button>
+
+   </>
   )
 }
 
