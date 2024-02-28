81eb9f27b995fc5875c22a6ccf8081530b69bfbe
marcinoles87 <marcinoles87@gmail.com>
2024-02-28 17:54:57 +0100

    work with code

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ import Jadlospis from './components/Jadlospis.js';
 import Main from './components/Main';
 import Navbars from './components/Navbars.js'
 import Button from 'react-bootstrap/esm/Button.js';
+import Footer from './components/Footer.js';
+
+
 import Grupa1 from './components/pages/Grupa1.js';
 import Grupa2 from './components/pages/Grupa2.js';
 import Grupa3 from './components/pages/Grupa3.js'
@@ -39,6 +42,7 @@ function App() {
           <Galeria></Galeria>
           <Jadlospis></Jadlospis>
           <Contact></Contact>
+          <Footer></Footer>
       </>
       }/>
       
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function Footer() {
+  return (
+    <div className='footer-container'>
+        <p>Przedszkole 111 Krakow 2024 Copyright Marcin Oleś</p>
+    </div>
+  )
+}
+
+export default Footer
