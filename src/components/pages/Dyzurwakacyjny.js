072727ceb163439aa9a8179a39cb8e0b51cd3a3b
marcinoles87 <marcinoles87@gmail.com>
2024-03-05 20:42:55 +0100

    work with code

--- src/components/pages/Dyzurwakacyjny.js
+++ src/components/pages/Dyzurwakacyjny.js
@@ -4,8 +4,8 @@ import './styleGrup.css';
 function Dyzurwakacyjny() {
   return (
     <div className='dyzur-container'>
-        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSe5pZOpgBpIglGxv4KakV6wnbureVmJwPPgquY0vAWK02VsXw/viewform?embedded=true" width="640" height="1082" frameborder="0" marginheight="0" marginwidth="0" title='formularz'>Ładuję…</iframe>
-    </div>
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSe5pZOpgBpIglGxv4KakV6wnbureVmJwPPgquY0vAWK02VsXw/viewform?embedded=true" width="640" height="1082" frameborder="0" marginheight="0" marginwidth="0">Ładuję…</iframe>   
+       </div>
   )
 }
 
