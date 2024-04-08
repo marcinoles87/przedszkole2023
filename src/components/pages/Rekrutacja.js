1e7952702ec0bb32a379282472587f0a55db6241
marcinoles87 <marcinoles87@gmail.com>
2024-04-08 21:57:50 +0200

    add pages and components

--- src/components/pages/Rekrutacja.js
+++ src/components/pages/Rekrutacja.js
@@ -8,50 +8,10 @@ function Rekrutacja() {
 
 <div className='rekrutacja-info'>
 
-<h1>REKRUTACJA DLA DZIECI ROZPOCZYNAJĄCYCH KORZYSTANIE z Przedszkola nr 111 „Pod Modrzewiem”
 
-OD 1 WRZEŚNIA 2024</h1>
-<p>
-<b>1 marca 2024 r.</b> zostanie uruchomiony elektroniczny serwis rekrutacyjny dla
-rodziców Formico ( krakow.formico.pl ). W serwisie tym będzie można
-zapoznać się z zasadami rekrutacji oraz pobrać wnioski o przyjęcie do
-przedszkola.
-</p>
-
-<p>
-<b>Od 1 do 29 marca 2024 r. </b>będzie odbywać się przyjmowanie wypełnionych
-i podpisanych wniosków.<br></br>
-Kolejność składania wniosków nie ma znaczenia !
-<br></br>
-Możliwości złożenia wniosku o przyjęcie do przedszkola:
-<br></br> druk wniosku będzie można pobrać w wersji papierowej z przedszkola w dniu 1 marca
-2024 r. od godz. 12:00. Należy go wypełnić, podpisać i wraz z załącznikami dostarczyć
-do przedszkola,
-<br></br> wniosek można wypełnić elektronicznie za pomocą platformy Formiko. Wypełniony
-wniosek można:
-o <br></br>-podpisać profilem zaufanym
-o <br></br>-wydrukować, podpisać i wraz z załącznikami dostarczyć do przedszkola.
-Wnioski muszą być podpisane przez obydwojga rodziców.
-</p>
-
-<p>
-<b>19 kwietnia 2024 r.</b> nastąpi ogłoszenie wyników rekrutacji (lista
-zakwalifikowanych i niezakwalifikowanych). Do rodziców, którzy podali we
-wniosku adres poczty elektronicznej zostanie wysłana w tej sprawie
-wiadomość e-mail.
-</p>
-<p>
- <b>Od 22 kwietnia do 7 maja 2024 r.</b> rodzice dzieci zakwalifikowanych do
-przedszkola muszą potwierdzić wolę przyjęcia poprzez wypełnienie
-i podpisanie druku, który będzie dostępny w kancelarii w godzinach od 7:00
-do 15:00. Nie potwierdzenie woli przyjęcia w wyznaczonym terminie będzie
-równoznaczne z rezygnacją z przyjęcia dziecka do naszego przedszkola.
-</p>
-
-<p>
-  <b>8 maja 2024 r. od godz. 12:00</b> nastąpi ogłoszenie list przyjętych i nieprzyjętych
-oraz liczby wolnych miejsc.    
-</p>
+ 
+<h1>Rekrutacja na dyżur wakacyjny zakończona.</h1>
+<p>Rodzice, którzy wyrazili chęć zapisu dziecka na dyżur wakacyjny w sierpniu 2024 r. proszeni są o zgłoszenie się do kancelarii w celu potwierdzenia woli przyjęcia poprzez wypełnienie i podpisanie druku. Kancelaria jest czynna codziennie w godzinach od 7:00 do 15:00" </p>
 
 
  
