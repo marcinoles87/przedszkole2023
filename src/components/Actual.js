7a9286304ffbb992f35b0b8a6d0c26c30af36eec
marcinoles87 <marcinoles87@gmail.com>
2024-06-27 19:06:19 +0200

    add section

--- src/components/Actual.js
+++ src/components/Actual.js
@@ -55,14 +55,14 @@ function Actual() {
 
 
 
-        {
-            title: "Jedzenie" ,
-            descirption : " informacje na temat żywienia " ,
-            imgUrl : jedzenie ,
-            info : 'W kontekście jedzenia w przedszkolu warto zwrócić uwagę, że posiłki powinny być odpowiednio zbilansowane pod względem wartości odżywczych i dostosowane do potrzeb rozwojowych dzieci. Ważne jest, aby promować zdrowe nawyki żywieniowe od najmłodszych lat, oferując świeże i różnorodne posiłki, które będą wspierać rozwój fizyczny i umysłowy maluchów. Ponadto, bezpieczeństwo żywności i higiena podczas przygotowywania posiłków w placówce przedszkolnej są kluczowe dla zapewnienia dobrej kondycji zdrowotnej dzieci i uniknięcia problemów związanych z zatruciami pokarmowymi. Dlatego od samego początku preferujemy zdrowe zbilansowane posiłki dla naszych podopiecznych przygotowane w naszej przedszkolnej kuchni . ',
-            link :''
+        // {
+        //     title: "Jedzenie" ,
+        //     descirption : " informacje na temat żywienia " ,
+        //     imgUrl : jedzenie ,
+        //     info : 'W kontekście jedzenia w przedszkolu warto zwrócić uwagę, że posiłki powinny być odpowiednio zbilansowane pod względem wartości odżywczych i dostosowane do potrzeb rozwojowych dzieci. Ważne jest, aby promować zdrowe nawyki żywieniowe od najmłodszych lat, oferując świeże i różnorodne posiłki, które będą wspierać rozwój fizyczny i umysłowy maluchów. Ponadto, bezpieczeństwo żywności i higiena podczas przygotowywania posiłków w placówce przedszkolnej są kluczowe dla zapewnienia dobrej kondycji zdrowotnej dzieci i uniknięcia problemów związanych z zatruciami pokarmowymi. Dlatego od samego początku preferujemy zdrowe zbilansowane posiłki dla naszych podopiecznych przygotowane w naszej przedszkolnej kuchni . ',
+        //     link :''
             
-        },
+        // },
 
         {
             title: "Dla rodzicow " ,
