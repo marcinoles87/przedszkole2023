30c475870bb0aeb22f723112c7c28e4f4bc9877a
marcinoles87 <marcinoles87@gmail.com>
2024-08-29 21:59:54 +0200

    add firebase

--- src/components/Main.js
+++ src/components/Main.js
@@ -49,20 +49,20 @@ function Main() {
           
           <Col > 
             <h2>OPŁATY ZA KORZYSTANIE Z PRZEDSZKOLA</h2>
-            <p><b>Od 1 sierpnia 2024 roku</b> zmianie ulegają opłaty za korzystanie z przedszkola:</p>
-            <p>Dzienna stawka żywieniowa wynosi:</p>
+            <p><b>Od 1 września 2024 roku</b> opłata za korzystanie z przedszkola wynosi:</p>
+            <p>Dzienna stawka żywieniowa:</p>
             <ul>
               <li> 3,6 zł - I śniadanie</li>
               <li>2,4 zł - II śniadanie</li>
               <li>6 zł - obiad</li>
-           
+  
 
 
             </ul>
-            <p>Miesięczna opłata za świadczenia wynosi: </p>
-            <p>• 1,30 zł za każdą dodatkową godzinę,</p> 
-            <p>• 0,65 zł dla rodzin objętych programem „Krakowska Karta Rodzinna 3 +” lub „Kraków dla Rodziny N” </p>
-            <p>• ponad 5 bezpłatnych godzin , miedzy 8:00-13:00</p>
+            <p>Miesięczna opłata za pobyt: </p>
+            <p>•bezpłatne od 8.00-13.00 oraz dla dzieci odbywających roczne przygotowanie przedszkolne</p>
+            <p>•1,44 zł/za godzinę dla pozostałych dzieci</p> 
+            <p>• 0,72zł dla rodzin objętych programem „Krakowska Karta Rodzinna 3 +” lub „Kraków dla Rodziny N” </p>
             
          </Col>
          
