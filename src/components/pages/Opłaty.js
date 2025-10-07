00c9b70688e6a36da53900ff72ac9a7e4ff1d279
marcinoles87 <marcinoles87@gmail.com>
2025-10-07 16:06:24 +0200

    add new href

--- src/components/pages/Opłaty.js
+++ src/components/pages/Opłaty.js
@@ -18,7 +18,7 @@ function Opłaty() {
             <p>•bezpłatne od 8.00-13.00 oraz dla dzieci odbywających roczne przygotowanie przedszkolne</p>
             <p>•1,44 zł/za godzinę dla pozostałych dzieci</p> 
             <p>• 0,72zł dla rodzin objętych programem „Krakowska Karta Rodzinna 3 +” lub „Kraków dla Rodziny N” </p>
-            <p><b>Numer konta do wpłat : 22 1020 2892 0000 5402 0590 4059</b></p>
+            {/* <p><b>Numer konta do wpłat : 22 1020 2892 0000 5402 0590 4059</b></p> */}
             <p>W tytule płatności prosimy wpisywać : OPŁATA ZA PRZEDSZKOLE NR 111 za ... (imię i nazwisko dziecka) ZA MIESIĄC ...</p>
 
     </div>
